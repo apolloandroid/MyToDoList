34895f4575e1ab43dafd7272f744d0219a1d2c62
apolloandroid <hmelevskih94@gmail.com>
2020-08-26 23:06:18 +0300

    Did some refactoring

--- app/src/main/java/com/example/mytodolist/MainActivity.kt
+++ app/src/main/java/com/example/mytodolist/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.mytodolist
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,21 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-    }
-
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> application.setTheme(R.style.SecondAppTheme)
-        }
-        return true
     }
 }
