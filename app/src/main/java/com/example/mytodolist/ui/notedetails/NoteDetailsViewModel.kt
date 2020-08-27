51cdd24258708228c2c1fd945e67f01c95948436
apolloandroid <hmelevskih94@gmail.com>
2020-08-27 23:11:28 +0300

    Overrided onCreate() function and added some LiveData objects

--- app/src/main/java/com/example/mytodolist/ui/notedetails/NoteDetailsViewModel.kt
+++ app/src/main/java/com/example/mytodolist/ui/notedetails/NoteDetailsViewModel.kt
@@ -1,17 +1,17 @@
 package com.example.mytodolist.ui.notedetails
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.mytodolist.repository.Repository
 import com.example.mytodolist.repository.database.Note
 import kotlinx.coroutines.*
 
 
 class NoteDetailsViewModel(
-    private val repository: Repository, application: Application, private val currentNoteId: Long
-) : AndroidViewModel(application) {
+    private val repository: Repository, context: Context, private val currentNoteId: Long
+) : ViewModel() {
     private var viewModelJob = Job()
     private val viewModelScope = CoroutineScope(Dispatchers.IO + viewModelJob)
 
@@ -19,7 +19,15 @@ class NoteDetailsViewModel(
     val currentNote: LiveData<Note>
         get() = _currentNote
 
-    fun showCurrentNoteText(currentNoteId: Long) {
+    private var _onNoteUpdated = MutableLiveData<Boolean>()
+    val onNoteUpdated: LiveData<Boolean>
+        get() = _onNoteUpdated
+
+    init {
+        showCurrentNoteText()
+    }
+
+    private fun showCurrentNoteText() {
         viewModelScope.launch {
             _currentNote.postValue(repository.getNote(currentNoteId))
         }
@@ -31,6 +39,7 @@ class NoteDetailsViewModel(
         viewModelScope.launch {
             repository.updateNote(_currentNote.value as Note)
         }
+        _onNoteUpdated.value = true
     }
 
     override fun onCleared() {
